3c40c71e05e0f3fb52efa70ac89c5c66f4865239
Imran Price <imranprice96@gmail.com>
2022-11-26 23:12:02 +1300

    Add css to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Form from "./components/Form";
+import "./App.css";
 
 function App() {
 	return (
-		<div>
+		<div className="main-body">
 			<Form />
 		</div>
 	);
